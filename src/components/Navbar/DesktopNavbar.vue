beabfd503f5784027e0a5b0d01da023a5db5e4e5
tnorthern <traynorthern@yahoo.com>
2021-11-11 17:16:49 -0600

    client only

--- src/components/Navbar/DesktopNavbar.vue
+++ src/components/Navbar/DesktopNavbar.vue
@@ -1,12 +1,14 @@
 <template>
   <nav class="hidden lg:flex flex-row ml-auto space-x-12 items-center font-bold">
     <div v-for="navLink in navLinks" :key="navLink.name">
-      <a
-        v-smooth-scroll
-        :href="navLink.href"
-        class=" scrollactive-item"
-        :class="[navLink.class, activeHash === navLink.href ? 'text-yellow-400' : 'hover:text-yellow-400 transition duration-150']"
-      >{{ navLink.name }}</a>
+      <client-only>
+        <a
+          v-smooth-scroll
+          :href="navLink.href"
+          class="scrollactive-item"
+          :class="[navLink.class, activeHash === navLink.href ? 'text-yellow-400' : 'hover:text-yellow-400 transition duration-150']"
+        >{{ navLink.name }}</a>
+      </client-only>
     </div>
   </nav>
 </template>
