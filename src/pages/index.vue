8122370016b1354b0a92102eef85b47560afcfb5
Tray Northern <traynorthern@yahoo.com>
2021-11-19 17:03:47 -0600

    Meta

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,3 +8,30 @@
     <Contact />
   </div>
 </template>
+
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+useHead({
+  title: 'EwayScooters',
+  meta: [
+    {
+      name: 'description',
+      content: 'We rent scooters to the general public for Recreation, Tours, Dates, and School Visitations. Riding with Eway LLC will show you the city like you\'ve never seen it before. Give us a call to rent your scooters.',
+    },
+    { name: 'og:title', content: 'Eway scooters' },
+    {
+      name: 'og:description',
+      content: 'We rent scooters to the general public for Recreation, Tours, Dates, and School Visitations. Riding with Eway LLC will show you the city like you\'ve never seen it before. Give us a call to rent your scooters.',
+    },
+    {
+      name: 'og:image',
+      content: 'https://lh3.googleusercontent.com/p/AF1QipPGecb1C2LnU4DrBObeRdYlseqZECx-tGusFiSW=w960-h960-n-o-v1',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon', type: 'image/x-icon', href: 'https://media.discordapp.net/attachments/874257099822075914/911391031898025994/e2.PNG?width=444&height=559',
+    },
+  ],
+})
+</script>
